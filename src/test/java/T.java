b5213119671ecc030eba5e8d255a1f4f9b6cc96d
799378666@qq.com <799378666@qq.com>
2017-03-05 22:30:21 +0800

    ...

--- src/test/java/T.java
+++ src/test/java/T.java
@@ -20,7 +20,7 @@ public class T {
         List<SmsTemplate> smsTemplateList = new ArrayList<SmsTemplate>();
         smsTemplateList.add(new SmsTemplate("SMS_26180244","您好${name}今天是${time}"));
 
-        SmsTool_Alidayu smsTool_alidayu = new SmsTool_Alidayu("燎火","SMS_5075620","23300185","8b5196bef2e1ebcf5d1f75503e2a4cd8",smsTemplateList);
+        SmsTool_Alidayu smsTool_alidayu = new SmsTool_Alidayu("燎火","SMS_5075620","23300185","8b5196bef2e1ebcf5d1f75123503e2a4cd8",smsTemplateList);
         smsTool_alidayu.sendSms("17681109309","您好呀今天是星期天");
     }
 }
